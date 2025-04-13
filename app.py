2d55919667edfb5ed6e813306dbeb7490ab10ef4
Divyansh <142193842+csdivyansh@users.noreply.github.com>
2025-04-14 01:13:54 +0530

    added ui for home page

--- app.py
+++ app.py
@@ -141,4 +141,4 @@ def delete_booking(booking_id):
     return redirect(url_for('view'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0', port = 8085)
+    app.run(host = '0.0.0.0')
